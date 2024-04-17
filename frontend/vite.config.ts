62e45ca1df263335fca6c3da1dddb8a5df84bdb4
tmdwoghd <tmdwoghd@naver.com>
2024-04-17 17:58:40 +0900

    ⚙️ config : PWA 설정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsConfigPaths from 'vite-tsconfig-paths'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
-})
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        
+      }
+   })],
+});
