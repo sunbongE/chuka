efc3298b4a8af0969ab5ef8904a8dcd11ebf85f9
박태호 <qkrxogh7@naver.com>
2024-04-22 17:27:23 +0900

    test: mongo Test

--- backend/notification/src/main/java/com/luckyseven/notification/controller/TestController.java
+++ backend/notification/src/main/java/com/luckyseven/notification/controller/TestController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/test")
@@ -16,8 +19,15 @@ public class TestController {
     @GetMapping("/save")
     public ResponseEntity<?> test(){
         Test ts = new Test();
-        ts.setId("testId");
-        ts.setData("저장되라");
+        ts.setId("testId2");
+        ts.setData("저장되라2");
+        // 현재 날짜를 가져옴
+        Date date = new Date();
+        // 날짜를 원하는 형식으로 변환
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String formattedDate = formatter.format(date);
+        ts.setTime(formattedDate);
+
         Test save = testRepo.save(ts);
         return ResponseEntity.ok().body(save);
     }
