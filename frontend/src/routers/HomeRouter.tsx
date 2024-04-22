bbffe98cd1205467867f37013e233e94fd6cf639
kanagyi95 <kanagyi95@gmail.com>
2024-04-22 17:17:37 +0900

    ✨ feat : 추카 등록 화면 생성

--- frontend/src/routers/HomeRouter.tsx
+++ frontend/src/routers/HomeRouter.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "@pages/login/LoginPage";
+import RollingCreatePage from "@/pages/rolling/RollingCreatePage";
 
 const HomeRouter = () => {
   return (
     <Routes>
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/make-chuka" element={<RollingCreatePage />} />
     </Routes>
   );
 };
