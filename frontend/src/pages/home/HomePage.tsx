2b58121ab061edb0180e4b25a3a45d1bb8c4e701
kanagyi95 <kanagyi95@gmail.com>
2024-04-23 13:53:17 +0900

    ✨ feat: 라디오 버튼 컴포넌트 구현 중

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,10 +1,11 @@
 import Button from "@/common/button";
 import styled from "styled-components";
 import { colors } from "@styles/theme";
-import Input from "@common/input"
+import Input from "@common/input";
 import { useState } from "react";
-import Label from "@common/label"
-
+import Label from "@common/label";
+import RadioButton from "@/common/radioButton";
+import { MdCake } from "react-icons/md";
 
 const Container = styled.div`
   display: flex;
@@ -17,22 +18,42 @@ const Wrap = styled.div`
 `;
 
 const HomePage = () => {
+  const [vvalue, setVvalue] = useState("");
 
-  const [vvalue, setVvalue] = useState("")
-  
-  const handleChange = (value:string) => {
+  const handleChange = (value: string) => {
     console.log(value);
-    setVvalue(value)
+    setVvalue(value);
+  };
 
-    
-
-  }
+  //라디오 버튼 테스트
+  const [value, setValue] = useState("");
+  const [selected, setSelected] = useState("");
+  const handleRadioChange = (value: string) => {
+    setSelected(value); // 라디오 버튼 선택값을 변경
+    console.log("버튼선택", value);
+  };
 
   return (
     <div>
-      <Label htmlFor={"id"}>사랑해요</Label>
-      <Input value={vvalue} id={"id"} placeholder={"아이디"} onInputChange={handleChange} onEnterKeyUp={handleChange} />
-      <Button children={"확인"} onClick={() => console.log("확인")}></Button>
+      {/* <Label htmlFor={"id"}>사랑해요</Label>
+      <Input
+        value={vvalue}
+        id={"id"}
+        placeholder={"아이디"}
+        onInputChange={handleChange}
+        onEnterKeyUp={handleChange}
+      />
+      <Button children={"확인"} onClick={() => console.log("확인")}></Button> */}
+      <RadioButton
+        value="option1"
+        checked={selected === "option1"}
+        onChange={() => handleRadioChange("option1")}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <MdCake />
+          <span>생일</span>
+        </div>
+      </RadioButton>
     </div>
   );
 };
--- frontend/src/routers/HomeRouter.tsx
+++ frontend/src/routers/HomeRouter.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "@pages/login/LoginPage";
+import HomePage from "@/pages/home/HomePage";
 
 const HomeRouter = () => {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />}/>
       <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
