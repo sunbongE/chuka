4d17143b4ec539d3bc97aca38bdadc02989df49a
김신영 <estumav@gmail.com>
2024-04-24 14:40:01 +0900

    ✨ feat: 회원정보 관리

--- backend/user/src/main/java/com/luckyseven/user/user/service/UserService.java
+++ backend/user/src/main/java/com/luckyseven/user/user/service/UserService.java
@@ -1,7 +1,14 @@
 package com.luckyseven.user.user.service;
 
+import com.luckyseven.user.user.dto.MyInfoDto;
+import com.luckyseven.user.user.dto.UserDto;
+
 public interface UserService {
 
     boolean isExistUser(String userId);
+
+    MyInfoDto getMyInfo(String userId);
+    UserDto getUser(String userId);
+    void deleteUser(String userId);
 
 }
--- backend/user/src/main/java/com/luckyseven/user/user/service/UserServiceImpl.java
+++ backend/user/src/main/java/com/luckyseven/user/user/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.luckyseven.user.user.service;
 
+import com.luckyseven.user.user.dto.MyInfoDto;
+import com.luckyseven.user.user.dto.UserDto;
+import com.luckyseven.user.user.entity.User;
 import com.luckyseven.user.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,5 +20,24 @@ public class UserServiceImpl implements UserService {
     @Override
     public boolean isExistUser(String userId) {
         return userRepository.findByUserId(userId) != null;
+    }
+
+    @Override
+    public MyInfoDto getMyInfo(String userId) {
+        User user = userRepository.findByUserId(userId);
+
+        return MyInfoDto.of(user);
+    }
+
+    @Override
+    public UserDto getUser(String userId) {
+        User user = userRepository.findByUserId(userId);
+
+        return UserDto.of(user);
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        // TODO: 알림 삭제, 펀딩 삭제, 롤링페이퍼 삭제, 회원 삭제
     }
 }
