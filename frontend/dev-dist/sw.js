c628241569b7989128ee303edbe9b501187be7b5
박태호 <qkrxogh7@naver.com>
2024-04-24 20:09:55 +0900

    test: 배포테스트

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.k2oqlq8hh8g"
+    "revision": "0.ietl8t8acuo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
