e14d07cde0d317c4c9648833e04b21bc25da699f
이현호 <lhh9799@naver.com>
2024-04-25 14:06:15 +0900

    Resolve S10P31C107-236 "Be amazon s 3 설정"

---
+++ backend/event/src/main/java/com/luckyseven/event/common/exception/BigFileException.java
@@ -0,0 +1,5 @@
+package com.luckyseven.event.common.exception;
+
+public class BigFileException extends Exception {
+    public BigFileException() {}
+}
--- backend/event/src/main/java/com/luckyseven/event/rollsheet/service/EventServiceImpl.java
+++ backend/event/src/main/java/com/luckyseven/event/rollsheet/service/EventServiceImpl.java
@@ -1,17 +1,22 @@
 package com.luckyseven.event.rollsheet.service;
 
 import com.github.f4b6a3.ulid.UlidCreator;
+import com.luckyseven.event.common.exception.BigFileException;
+import com.luckyseven.event.common.exception.EmptyFileException;
+import com.luckyseven.event.common.exception.NotValidExtensionException;
 import com.luckyseven.event.rollsheet.dto.CreateEventDto;
 import com.luckyseven.event.rollsheet.dto.EventDto;
 import com.luckyseven.event.rollsheet.entity.Event;
 import com.luckyseven.event.rollsheet.entity.EventType;
 import com.luckyseven.event.rollsheet.entity.Theme;
 import com.luckyseven.event.rollsheet.repository.EventRepository;
+import com.luckyseven.event.util.FileService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -20,19 +25,22 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
 
+    private final FileService fileService;
+
     private final EventRepository eventRepository;
 
     @Override
-    public EventDto createEvent(CreateEventDto eventDto) {
+    public EventDto createEvent(CreateEventDto eventDto) throws EmptyFileException, BigFileException, NotValidExtensionException, IOException {
         Event event = new Event();
         event.setUserId(eventDto.getUserId());
         event.setPageUri(UlidCreator.getUlid().toString());
         event.setType(EventType.valueOf(eventDto.getType()));
         event.setTitle(eventDto.getTitle());
         event.setDate(eventDto.getDate());
-        event.setBanner(eventDto.getBanner());
-        // 썸네일
-        //  event.setBannerThumbnail();
+        //Amazon S3에 배너 이미지 업로드 후 경로 저장
+        String[] bannerPath = fileService.uploadBannerImageToAmazonS3(eventDto.getBannerImage());
+        event.setBanner(bannerPath[0]);
+        event.setBannerThumbnail(bannerPath[1]);
         event.setTheme(Theme.valueOf(eventDto.getTheme()));
         event.setVisibility(eventDto.getVisibility());
         event.setCreateTime(LocalDateTime.now());
