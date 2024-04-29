af493f212ca7b6336563e96b146158cbbc079a81
kanagyi95 <kanagyi95@gmail.com>
2024-04-29 23:34:13 +0900

    ✨ feat : token 마지막 도전..

--- frontend/src/pages/redirect/LoginRedirectHandler.tsx
+++ frontend/src/pages/redirect/LoginRedirectHandler.tsx
@@ -3,38 +3,42 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "@/utils/requestMethods";
 
-const KAKAO_API_KEY = import.meta.env.VITE_KAKAO_API_KEY;
-const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+const KAKAO_API_KEY = process.env.VITE_KAKAO_API_KEY;
+const REDIRECT_URI = process.env.VITE_REDIRECT_URI;
 
 const LoginRedirectHandler = () => {
   const navigate = useNavigate();
   const code = new URLSearchParams(window.location.search).get("code");
 
   useEffect(() => {
-    if (code !== null) {
-      getToken(code);
-    }
-  }, [code, navigate]);
-
-  const getToken = (code: string) => {
-    const url = "https://kauth.kakao.com/oauth/token";
-    const params = new URLSearchParams();
-    params.append("grant_type", "authorization_code");
-    params.append("client_id", KAKAO_API_KEY);
-    params.append("redirect_uri", REDIRECT_URI);
-    params.append("code", code);
-
-    axios
-      .post(url, code)
-      .then((res) => {
-        console.log(res.data);
-        localStorage.setItem("accessToken", res.data.access_token);
-        navigate("/");
+    if (code) {
+      getToken(code).then((res) => {
+        console.log(res.access_token);
       })
-      .catch((err) => {
-        console.error(err);
-      });
+    }
+  }, []);
+
+  const getToken = async (code: string) => {
+    const response = await fetch(
+      `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${KAKAO_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8',
+        },
+      }
+    );
+    return response.json()
+      // .post(`${BASE_URL}/auth/login/kakao`, code)
+      // .then((res) => {
+      //   console.log(res.data);
+      //   localStorage.setItem("accessToken", res.data.access_token);
+      //   navigate("/");
+      // })
+      // .catch((err) => {
+      //   console.error(err);
+      // });
   };
+  
   return (
     <>
       <p>로그인 처리 중입니다.</p>
