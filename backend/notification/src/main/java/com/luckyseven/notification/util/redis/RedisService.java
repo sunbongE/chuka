eabbf35a90c685b72f0849ea424af0ff09681f16
박태호 <qkrxogh7@naver.com>
2024-04-29 10:51:29 +0900

    ✨ feat: 알림 시큐리티 적용

---
+++ backend/notification/src/main/java/com/luckyseven/notification/util/redis/RedisService.java
@@ -0,0 +1,15 @@
+package com.luckyseven.notification.util.redis;
+
+import java.time.Duration;
+
+public interface RedisService {
+    boolean save(String id, String token, Duration duration);
+    boolean saveRefreshToken(String id, String token);
+    boolean saveLogoutToken(String accessToken);
+
+    String getValues(String key);
+
+    boolean delete(String key);
+
+    boolean hasKey(String key);
+}
