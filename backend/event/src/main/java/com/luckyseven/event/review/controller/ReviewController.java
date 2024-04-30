a92b5b286655e24ba8bd302fe74770494c3e687d
김신영 <estumav@gmail.com>
2024-04-30 11:48:53 +0900

    Develop

--- backend/event/src/main/java/com/luckyseven/event/review/controller/ReviewController.java
+++ backend/event/src/main/java/com/luckyseven/event/review/controller/ReviewController.java
@@ -23,6 +23,12 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+    @GetMapping("/test")
+    public ResponseEntity<String> test(
+    ) {
+        return ResponseEntity.status(200).body("review Controller");
+    }
+
     @GetMapping("/")
     @Operation(summary = "리뷰 조회", description = "리뷰를 최신순으로 3개 조회한다.")
     @ApiResponses({
