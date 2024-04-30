15257e69e51f8fc11e5b5e699aff112fdebc86e8
김신영 <estumav@gmail.com>
2024-04-30 15:28:06 +0900

    ✨ feat : filter 검증

--- backend/SCG/src/main/java/com/luckyseven/SCG/util/JwtUtil.java
+++ backend/SCG/src/main/java/com/luckyseven/SCG/util/JwtUtil.java
@@ -1,6 +1,8 @@
 package com.luckyseven.SCG.util;
 
+import com.luckyseven.SCG.util.redis.RedisService;
 import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +14,6 @@ import java.nio.charset.StandardCharsets;
 @Component
 public class JwtUtil {
     private final SecretKey secretKey;
-
 
     public JwtUtil(@Value("${secret-key}") String secret) {
         secretKey = new SecretKeySpec(
@@ -31,6 +32,15 @@ public class JwtUtil {
                 .parseSignedClaims(token)
                 .getPayload()
                 .get("id", String.class);
+    }
+
+    public String getType(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("Type", String.class);
     }
 
 }
