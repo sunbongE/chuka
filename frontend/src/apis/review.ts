4e542dbcd4a67c8a9a35d6437c415285115ca55a
tmdwoghd <tmdwoghd@naver.com>
2024-05-02 17:15:21 +0900

    ✨ feat : 리뷰 조회1

--- frontend/src/apis/review.ts
+++ frontend/src/apis/review.ts
@@ -1,15 +1,22 @@
 import { authRequest } from "@/utils/requestMethods";
-import { error } from "console";
 
 const url = '/api/v1/reviews'
 
 // 리뷰 작성
 
-export const createReview = async (params:object) => {
+export const createReview = async (params:object) : Promise<any> => {
     authRequest
         .post(`${url}`, params)
         .then(res => {
             return res.data
         })
         .catch(error => console.error(error))
+}
+
+
+export const fetchReview = async () : Promise<any> => {
+    authRequest
+    .get(`${url}`).then(res => {return res.data})
+    .catch(error => console.error(error))
+
 }
