8e707e0293a3b0b0a19b8bddd859cfb960714179
김지연 <flyhigh9870@gmail.com>
2024-05-03 04:01:46 +0900

    ✨ feat: Crawling서버 RabbitMQ 적용

--- backend/crawling/app.py
+++ backend/crawling/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, request, jsonify
 import py_eureka_client.eureka_client as eureka_client
 from views.crawling_views import app_views
+from views.rabbitmq_client import receive_message
+from threading import Thread
 
 rest_port = 8085
 
 app = Flask(__name__)
 
+
 def create_app():
-    #eureka_client.init(eureka_server="http://admin:5678@localhost:8761/eureka",
-    #            app_name="crawling",
-    #            instance_port=rest_port)
+    eureka_client.init(eureka_server="http://luckyseven:cnzkcnzk7!@k10c107.p.ssafy.io:8761/eureka",
+                app_name="crawling",
+                instance_port=rest_port)
+
     app = Flask(__name__)
     app.register_blueprint(app_views)
     return app
 
 if __name__ == "__main__":
+    thread = Thread(target=receive_message)
+    thread.start()
     app = create_app()
     app.run(host='0.0.0.0', port = rest_port)
