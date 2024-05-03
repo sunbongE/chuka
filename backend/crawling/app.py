9b2014fa4e74b274703b936e372203b81b7b5045
김지연 <flyhigh9870@gmail.com>
2024-05-03 16:32:55 +0900

    🐛 fix: 메세지 받는 위치를 변경

--- backend/crawling/app.py
+++ backend/crawling/app.py
@@ -13,13 +13,12 @@ def create_app():
     # eureka_client.init(eureka_server="http://luckyseven:cnzkcnzk7!@k10c107.p.ssafy.io:8761/eureka",
     #             app_name="crawling",
     #             instance_port=rest_port)
-
+    thread = Thread(target=receive_message)
+    thread.start()
     app = Flask(__name__)
     app.register_blueprint(app_views)
     return app
 
 if __name__ == "__main__":
-    thread = Thread(target=receive_message)
-    thread.start()
     app = create_app()
     app.run(host='0.0.0.0', port = rest_port)
