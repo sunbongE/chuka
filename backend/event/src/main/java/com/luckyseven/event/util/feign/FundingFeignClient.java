d41b282203ba55d685a8aa2e7e73a11ab04cfe9b
김신영 <estumav@gmail.com>
2024-05-05 18:04:40 +0900

    ✨ feat : feign client 추가

---
+++ backend/event/src/main/java/com/luckyseven/event/util/feign/FundingFeignClient.java
@@ -0,0 +1,13 @@
+package com.luckyseven.event.util.feign;
+
+import feign.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
+@FeignClient(name = "fundingClient", url = "http://k10c107.p.ssafy.io:8083/api/v1/fundings")
+public interface FundingFeignClient {
+
+    @DeleteMapping("/eventId/{eventId}")
+    Response deleteFundingByEventId(int eventId);
+
+}
