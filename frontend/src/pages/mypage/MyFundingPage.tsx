38c53ab48176a304f284b61e2033c5371cae3b81
kanagyi95 <kanagyi95@gmail.com>
2024-05-07 23:34:01 +0900

    ✨ feat : 마이펀딩 api 연결 중

--- frontend/src/pages/mypage/MyFundingPage.tsx
+++ frontend/src/pages/mypage/MyFundingPage.tsx
@@ -2,13 +2,38 @@ import Header from "@common/header";
 import Navbar from "@common/navbar";
 import FundingNull from "@/components/mypage/MyFundingNull";
 import MyFunding from "@/components/mypage/MyFunding/idex";
+import { useEffect, useState } from "react";
+import { fetchMyFundings } from "@/apis/funding";
+
+interface MyFundingProps {
+  fundingId: number;
+  introduce: string;
+  fundingResult: string;
+  productImage?: string;
+  startDate: string;
+  endDate: string;
+}
 
 const MyFundingPage = () => {
+  const [values, setValues] = useState<MyFundingProps[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetchMyFundings();
+        setValues(response);
+        console.log("펀딩정보", values);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Header children="나의 펀딩" />
-      <MyFunding />
-      <FundingNull />
+      {values.length > 0 ? <MyFunding /> : <FundingNull />}
       <Navbar current="mypage" />
     </div>
   );
