9819e2404360e029cef232ebb532d9c9e58864a3
김지연 <flyhigh9870@gmail.com>
2024-05-09 07:23:53 +0900

    ✨ feat: 결제 검증 로직 추가

---
+++ backend/funding/src/main/java/com/luckyseven/funding/service/TransactionService.java
@@ -0,0 +1,10 @@
+package com.luckyseven.funding.service;
+
+import com.siot.IamportRestClient.exception.IamportResponseException;
+
+import java.io.IOException;
+
+public interface TransactionService {
+    String verifiyAndSavePayment (String transactionId, String pgId, Integer amount);
+    Boolean cancelPayment (String pgId);
+}
