786857a9c2d5e21afa42d71616afcfc2d4e89972
이현호 <lhh9799@naver.com>
2024-05-08 15:37:01 +0900

    🐛 fix : 펀딩 서버 SecurityConfig.java 롤백

--- backend/funding/src/main/java/com/luckyseven/funding/config/SecurityConfig.java
+++ backend/funding/src/main/java/com/luckyseven/funding/config/SecurityConfig.java
@@ -89,7 +89,6 @@ public class SecurityConfig {
                 .authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/swagger-resources/**", "/v3/api-docs/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
-                        .requestMatchers("/actuator/**/**").permitAll()
                         .requestMatchers("/api/v1/fundings/**").permitAll()
                         .requestMatchers( "/swagger-ui/**").hasRole("ADMIN")
                         .anyRequest().authenticated());
