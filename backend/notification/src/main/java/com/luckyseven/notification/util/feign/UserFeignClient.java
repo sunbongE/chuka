5f8529096283348baf0e3a2c4195b60fe6919850
박태호 <qkrxogh7@naver.com>
2024-05-08 16:31:52 +0900

    fix : feign 요청주소 변경

--- backend/notification/src/main/java/com/luckyseven/notification/util/feign/UserFeignClient.java
+++ backend/notification/src/main/java/com/luckyseven/notification/util/feign/UserFeignClient.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
-//@FeignClient(name = "usersClient", url = "http://ec2-43-203-200-59.ap-northeast-2.compute.amazonaws.com:8081/api/v1")
-@FeignClient(name = "usersClient", url = "http://localhost:8081/api/v1")
+@FeignClient(name = "usersClient", url = "http://ec2-43-203-200-59.ap-northeast-2.compute.amazonaws.com:8081/api/v1")
+//@FeignClient(name = "usersClient", url = "http://localhost:8081/api/v1")
 public interface UserFeignClient {
 
     @PostMapping("/users/notifications/fcm-token")
