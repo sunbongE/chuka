39dde782c7b871a448c38ecacb7159f4c11cb898
김신영 <estumav@gmail.com>
2024-05-09 14:12:07 +0900

    🧪 test : 카카오 회원가입 테스트코드 작성

--- backend/user/src/test/java/com/luckyseven/user/auth/service/AuthServiceImplTest.java
+++ backend/user/src/test/java/com/luckyseven/user/auth/service/AuthServiceImplTest.java
@@ -1,28 +1,82 @@
-//package com.luckyseven.user.auth.service;
-//
-//import com.luckyseven.user.auth.dto.KakaoUserDto;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit.jupiter.SpringExtension;
-//
-//@ExtendWith(SpringExtension.class)
-//@SpringBootTest
-////@TestPropertySource("classpath:/application.properties")
-//class AuthServiceImplTest {
-//
-//    AuthService authService;
-//
-//    @Test
-//    void getToken() {
-//        authService.getKakaoToken("FsNL6rJ3DEqBIPOtybtQTTThaunMDUT7Xol5SnC9IzJ8JOiMWltRBTaR2oUKPXTaAAABjwlf1qgWphHJzwXJqw");
-//    }
-//
-//    @Test
-//    void joinOrLoginForKakao() {
-////        KakaoUserDto dto = new KakaoUserDto();
-////        dto.setId(3448548097L);
-////        dto.setConnectedAt("2024-04-23T08:57:58Z");
-////        authService.joinOrLoginForKakao(dto);
-//    }
-//}
+package com.luckyseven.user.auth.service;
+
+import com.luckyseven.user.auth.dto.KakaoUserDto;
+import com.luckyseven.user.user.dto.UserDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static com.luckyseven.user.user.entity.Roles.ROLE_USER;
+
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+class AuthServiceImplTest {
+
+//    @Mock
+    @Autowired
+    AuthService authService;
+
+    @Test
+    void getToken() {
+    }
+
+    @Test
+    void joinOrLoginForKakao() {
+    }
+
+    @Test
+    void getKakaoToken() {
+    }
+
+    @Test
+    void getKakaoUserInfo() {
+    }
+
+    @Test
+    @DisplayName("카카오 회원가입")
+    void join() {
+        //
+        KakaoUserDto userDto = new KakaoUserDto();
+        userDto.setId(1L);
+        KakaoUserDto.Properties properties = new KakaoUserDto.Properties();
+        properties.setNickname("테스트_닉네임");
+        userDto.setProperties(properties);
+
+        //
+        UserDto join = authService.join(userDto);
+
+        //
+        UserDto test = UserDto.of(userDto);
+        test.setJoinDate(join.getJoinDate());
+        test.setRole(ROLE_USER);
+
+        Assertions.assertThat(join).isEqualTo(test);
+    }
+
+    @Test
+    void issueAccessToken() {
+    }
+
+    @Test
+    void testIssueAccessToken() {
+    }
+
+    @Test
+    void issueRefreshToken() {
+    }
+
+    @Test
+    void testIssueRefreshToken() {
+    }
+
+    @Test
+    void reIssueAccessTokenWithRefreshToken() {
+    }
+}
