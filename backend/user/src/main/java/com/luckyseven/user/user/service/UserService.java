ada6e4355b4e72f90c82790e9e6deafd34a6d20b
김신영 <estumav@gmail.com>
2024-05-10 09:28:35 +0900

    🧪 test : user 테스트코드 추가

--- backend/user/src/main/java/com/luckyseven/user/user/service/UserService.java
+++ backend/user/src/main/java/com/luckyseven/user/user/service/UserService.java
@@ -3,6 +3,7 @@ package com.luckyseven.user.user.service;
 import com.luckyseven.user.user.dto.DeduplicatedUsersIdDto;
 import com.luckyseven.user.user.dto.MyInfoDto;
 import com.luckyseven.user.user.dto.UserDto;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
@@ -15,7 +16,7 @@ public interface UserService {
     UserDto getUser(String userId);
     void deleteUser(String userId, String accessToken);
 
-    void saveFcmToken(String userId, String token) throws SQLIntegrityConstraintViolationException;
+    void saveFcmToken(String userId, String token) throws DataIntegrityViolationException;
     List<String> getUserFcmToken(String userId);
 
     void logout(String accessToken);
