a828c973d8f6cdb08400429a305c52c52d6f16ad
김신영 <estumav@gmail.com>
2024-05-10 12:09:27 +0900

    ✨ feat : 내 이벤트 검색 조건 추가

--- backend/event/src/main/java/com/luckyseven/event/rollsheet/repository/EventRepository.java
+++ backend/event/src/main/java/com/luckyseven/event/rollsheet/repository/EventRepository.java
@@ -3,6 +3,7 @@ package com.luckyseven.event.rollsheet.repository;
 import com.luckyseven.event.rollsheet.entity.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface EventRepository extends JpaRepository<Event, Integer> {
@@ -11,5 +12,8 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
     List<Event> findByUserId(String userId);
     int countByVisibility(boolean visibility);
     int countByUserId(String userId);
+    int countByUserIdAndDateGreaterThanEqual(String userId, LocalDate date);
+    int countByUserIdAndDateGreaterThanEqualAndTitleContaining(String userId, LocalDate date, String title);
+    int countByUserIdAndTitleContaining(String userId, String title);
 
 }
