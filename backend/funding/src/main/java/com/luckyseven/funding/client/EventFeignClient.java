2ac5ea285d011249e1c1ed87a664e18201b4767c
Hyunho Lee <lhh9799@gmail.com>
2024-05-11 19:53:48 +0900

    ✨ feat : 펀딩 단건 조회 - 펀딩 참여자의 프로필 이미지 출력 구현

--- backend/funding/src/main/java/com/luckyseven/funding/client/EventFeignClient.java
+++ backend/funding/src/main/java/com/luckyseven/funding/client/EventFeignClient.java
@@ -1,10 +1,11 @@
-package com.luckyseven.funding.util;
+package com.luckyseven.funding.client;
 
 import com.luckyseven.funding.dto.EventDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+//TODO: application.properties에 URL 추가
 @FeignClient(name = "eventclient", url = "http://k10c107.p.ssafy.io:8084/api/v1/events/")
 public interface EventFeignClient {
     //이벤트 정보 조회
---
+++ backend/funding/src/main/java/com/luckyseven/funding/client/UserFeignClient.java
@@ -0,0 +1,14 @@
+package com.luckyseven.funding.client;
+
+import com.luckyseven.funding.dto.UserDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//TODO: 테스트 중
+@FeignClient(name = "userclient", url = "${feignclient.user-baseUrl-v1}")
+public interface UserFeignClient {
+    //회원 정보 조회
+    @GetMapping("{userId}")
+    UserDto getUser(@PathVariable String userId);
+}
