827bef36feea53c405c470de1cdf8932c8ff7f07
kanagyi95 <kanagyi95@gmail.com>
2024-05-13 13:20:45 +0900

    🐛 fix : 알림 type 펀딩 부분 삭제 + 리프레시 토큰 테스트

--- frontend/src/apis/event.ts
+++ frontend/src/apis/event.ts
@@ -1,35 +1,39 @@
 import axios, { AxiosResponse } from "axios";
+import { refresh } from "./auth";
 
 const url = `https://chuka.kr/api/v1`;
 const local = "/domain";
-const accessToken = localStorage.getItem("access_token");
 
 // 이벤트 등록
 export const createEventReg = async (formdata: any) => {
+  let accessToken = localStorage.getItem("access_token");
   try {
     const response: AxiosResponse = await axios
       .post(`${url}/events`, formdata, {
         headers: {
           "Content-Type": "multipart/form-data",
-          // Authorization: `${accessToken}`,
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjM0NTI2NTk1NDMiLCJuaWNrbmFtZSI6Iuuwle2DnO2YuCIsInJvbGUiOiJST0xFX1VTRVIiLCJ0eXBlIjoiQVRLIiwiaWF0IjoxNzE1MDU1MzgwLCJleHAiOjE3MTUzMTQ1ODB9.yuwt2nCDD2VEkvJm1ZB-aFNfkFEFON8EeXYjJN6YlLI`,
+          Authorization: `${accessToken}`,
         },
       })
       .then((e) => {
         return response.data;
       })
       // 유효성 검사 예외처리
-      .catch((e:any) => {
+      .catch((e: any) => {
         if (e.response.status === 413) {
           alert("이미지 용량은 20MB 이하만 가능합니다.");
           return;
         } else if (e.response.status === 415) {
           alert("지원하지 않는 확장자입니다.(jpg,png,jpeg,gif,webp 만 가능)");
           return;
-        } else if (e.response.status === 401) {
-          console.log('data', e.response.data)
-          console.log('body', e.response.body)
-        } 
+        } else if (e.response.status === 401 && e.response.data === "EXPIRED") {
+          try {
+            const accessToken = refresh();
+            return createEventReg(formdata);
+          } catch (err) {
+            console.error(err);
+          }
+        }
       });
   } catch (err) {
     console.error(err);
@@ -87,6 +91,7 @@ export const fetchMyEventList = async ({
   size: number;
   word?: string;
 }) => {
+  let accessToken = localStorage.getItem("access_token");
   try {
     const response = await axios.get(`${url}/events/me`, {
       params: {
