f4252d4b04ac19677f7695a4296080471d66996a
kanagyi95 <kanagyi95@gmail.com>
2024-05-14 17:20:20 +0900

    ✨ feat : 결제수단 라디오버튼 추가

--- frontend/src/components/payment/MethodSection/MethodSection.styled.tsx
+++ frontend/src/components/payment/MethodSection/MethodSection.styled.tsx
@@ -7,11 +7,10 @@ export const Container = styled.div`
   width: 100%;
   gap: 20px;
 `;
+
 export const Title = styled.div`
-
-font-size: 1.5em;
-font-weight: 600;
-
+  font-size: 1.5em;
+  font-weight: 600;
 `;
 
 export const Button = styled.button<{ $active: boolean }>`
@@ -25,10 +24,13 @@ export const Button = styled.button<{ $active: boolean }>`
     ${(props) => (props.$active ? colors.mainPink : colors.gray)};
 `;
 
-export const Img = styled.img<{ $active: boolean }>`
+export const Radio = styled.input`
+  cursor: pointer;
+  margin-right: 10px;
+`;
+
+export const Img = styled.img`
   width: 20%;
   height: 20%;
-  border: 2px solid
-    ${(props) => (props.$active ? colors.mainPink : colors.gray)};
   border-radius: 35px;
 `;
