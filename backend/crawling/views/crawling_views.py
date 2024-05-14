8300f44bdf335f23bfd06ce1187eb0ff22121bfe
김지연 <flyhigh9870@gmail.com>
2024-05-14 14:25:16 +0900

    ✨ feat: 쿠팡 크롤링 추가

--- backend/crawling/views/crawling_views.py
+++ backend/crawling/views/crawling_views.py
@@ -1,20 +1,12 @@
 from flask import Blueprint, request, jsonify
 from utils.utils import extract_shopping_info
 from utils.selectors import get_selectors
-from views.rabbitmq_client import send_message
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# MQ 쓰지않고 크롤링이 제대로 되고 있는 지 확인하기 위한 api
 @app_views.route('/crawling/hello', methods=['GET'])
 def hello():
     return jsonify({"message": "HELLO!! This is Crawling Server"})
-
-@app_views.route('/crawling/rabbit/<param>', methods=['GET'])
-def rabbit_test(param):
-    info = {'productImageUrl': 'https://cdn.011st.com/11dims/resize/600x600/quality/75/11src/pd/v2/2/4/9/7/6/6/KCDQE/6736249766_B.jpg', 'productPrice': 1698400, 'productName': '갤럭시 자급제 SM-S928N', 'fundingId': 28, 'status': 200, 'message': '성공', 'userId': '3460091535'}
-    info['productName'] = param
-    print(info)
-    send_message(info)
-    return jsonify({"message": info})
 
 @app_views.route("/crawling", methods=["POST"])
 def extract_image():
@@ -30,7 +22,7 @@ def extract_image():
     if "error" in info:
         return jsonify(info), 500
     else:
-        if info.get("productImageUrl") and info.get("productName") and info.get("productPrice"):
+        if info.get("productImageUrl") and info.get("productName"):
             return jsonify(info)
         else:
             info["message"] = "구매 상세 페이지를 정확히 입력해주세요."
