f90240a92eaad3c83f14d2a7d57328efad3da521
김신영 <estumav@gmail.com>
2024-05-16 17:01:57 +0900

    ✨ feat : controller test class 생성

--- backend/user/src/test/java/com/luckyseven/user/auth/controller/AuthControllerTest.java
+++ backend/user/src/test/java/com/luckyseven/user/auth/controller/AuthControllerTest.java
@@ -1,8 +1,14 @@
 package com.luckyseven.user.auth.controller;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
 import static org.assertj.core.api.Assertions.*;
 
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
 class AuthControllerTest {
 
     @Test
